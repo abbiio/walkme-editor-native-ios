2d64bd2c9ae4a8d942b442ef64a9aa6d2cafa9c1
Guy Sharony <guy.s@walkme.com>
2026-03-12 15:33:07 +0200

    new package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,29 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-// WalkMeEditor iOS SDK 0.1.0
-
 let package = Package(
     name: "WalkMeEditor",
     platforms: [
         .iOS(.v14)
     ],
     products: [
-        .library(name: "WalkMeEditor", targets: ["WalkMeEditorBinary"])
+        .library(name: "WalkMeEditor", targets: ["WalkMeEditor"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.6.0")
     ],
     targets: [
         .binaryTarget(
             name: "WalkMeEditorBinary",
-            url: "https://github.com/abbiio/walkme-editor-native-ios/releases/download/0.1.0/WalkMeEditor.xcframework.zip",
-            checksum: "6fab0061994881eebed33f78ca6eecbef2cb94754874f5d73a41eb53c72266c3"
+            url: "https://github.com/abbiio/walkme-editor-native-ios/releases/download/1.0.0/WalkMeEditor.xcframework.zip",
+            checksum: "70f274018f68760eccfbcd62893967056f017950c0c58373e2e6a84d569d5fbb"
+        ),
+        .target(
+            name: "WalkMeEditor",
+            dependencies: [
+                "WalkMeEditorBinary",
+                .product(name: "Lottie", package: "lottie-spm")
+            ]
         )
     ]
 )

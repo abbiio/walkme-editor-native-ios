712d2f273ce6b4736405bd714a2be277cd3c4929
Guy Sharony <guy.s@walkme.com>
2026-03-12 17:09:39 +0200

    new package

--- Package.swift
+++ Package.swift
@@ -9,20 +9,10 @@ let package = Package(
     products: [
         .library(name: "WalkMeEditor", targets: ["WalkMeEditor"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.6.0")
-    ],
     targets: [
         .binaryTarget(
-            name: "WalkMeEditorBinary",
-            path: "WalkMeEditor.xcframework"
-        ),
-        .target(
             name: "WalkMeEditor",
-            dependencies: [
-                "WalkMeEditorBinary",
-                .product(name: "Lottie", package: "lottie-spm")
-            ]
+            path: "WalkMeEditor.xcframework"
         )
     ]
 )
